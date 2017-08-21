318aaca5ee5d20c0924351fcef9b55fb2c85f679
Luis Pallares <thepanchi@gmail.com>
2017-08-21 12:38:04 +0200

    add config folder

---
+++ config/enviroment_variables.sample.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+export ES_HOST=http://localhost:9200/
+export ES_USER=elastic
+export ES_PASSWORD=changeme
+export ES_ACLED_INDEX=acled
+export SLACK_NOTIFICATIONS_ENABLED=1
+export SLACK_NOTIFICATIONS_URL=https://hooks.slack.com/services/XXXX/YYYYY/ZZZZZ
