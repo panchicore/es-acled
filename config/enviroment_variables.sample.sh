444b5cf324b3d7846922c6308d901710a897e180
Luis Pallares <thepanchi@gmail.com>
2017-08-23 11:21:46 +0200

    use specific env var names

--- config/enviroment_variables.sample.sh
+++ config/enviroment_variables.sample.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-export ES_HOST=http://localhost:9200/
-export ES_USER=elastic
-export ES_PASSWORD=changeme
+export ES_ACLED_HOST=http://localhost:9200/
+export ES_ACLED_USER=elastic
+export ES_ACLED_PASSWORD=changeme
 export ES_ACLED_INDEX=acled
-export SLACK_NOTIFICATIONS_ENABLED=1
-export SLACK_NOTIFICATIONS_URL=https://hooks.slack.com/services/XXXX/YYYYY/ZZZZZ
+export ACLED_SLACK_NOTIFICATIONS_ENABLED=1
+export ACLED_SLACK_NOTIFICATIONS_URL=https://hooks.slack.com/services/XXXX/YYYYY/ZZZZZ
