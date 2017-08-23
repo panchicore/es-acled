883c4a52b70a30338f2779361ff7ab447611a3eb
Luis Pallares <thepanchi@gmail.com>
2017-08-23 11:22:50 +0200

    simpler indice creator

--- create_index.py
+++ create_index.py
@@ -1,25 +1,23 @@
 import requests
-import os
-
+import es
 from requests.auth import HTTPBasicAuth
 
-ES_HOST = os.environ.get("ES_HOST")
-ES_USER = os.environ.get("ES_USER")
-ES_PASSWORD = os.environ.get("ES_PASSWORD")
-ES_ACLED_INDEX = os.environ.get("ES_ACLED_INDEX")
-URL = ES_HOST + ES_ACLED_INDEX
-
 def create_indice(delete=None):
+    """
+
+    :param delete:
+    :return:
+    """
 
     if delete:
-        requests.delete(URL, auth=HTTPBasicAuth(ES_USER, ES_PASSWORD))
+        requests.delete(es.URL, auth=HTTPBasicAuth(es.ES_USER, es.ES_PASSWORD))
 
     template = open("elasticsearch/acled-template.json").read()
-    res = requests.put(URL, data=template, auth=HTTPBasicAuth(ES_USER, ES_PASSWORD))
+    res = requests.put(es.URL, data=template, auth=HTTPBasicAuth(es.ES_USER, es.ES_PASSWORD))
 
     print res.content
 
 
 if __name__ == "__main__":
-    delete = raw_input("Want to delete the actual '{0}' index? Y/N: ".format(ES_ACLED_INDEX)) == "Y"
+    delete = raw_input("Want to delete the actual '{0}' index? Y/N: ".format(es.ES_ACLED_INDEX)) == "Y"
     create_indice(delete)
